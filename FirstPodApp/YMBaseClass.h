f8587126666b143f2aa55cba562bd3f44b07b785
明明 <nslogyanmingyue@126.com>
2018-11-06 10:46:46 +0800

    Release 1.0.0

---
+++ FirstPodApp/YMBaseClass.h
@@ -0,0 +1,16 @@
+//
+//  YMBaseClass.h
+//  Masonry
+//
+//  Created by my y on 2018/11/1.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YMBaseClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FirstPodApp/YMBaseClass.m
@@ -0,0 +1,12 @@
+//
+//  YMBaseClass.m
+//  Masonry
+//
+//  Created by my y on 2018/11/1.
+//
+
+#import "YMBaseClass.h"
+
+@implementation YMBaseClass
+
+@end
